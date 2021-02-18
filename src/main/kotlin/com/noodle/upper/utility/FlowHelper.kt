7ddf5c460f8bd57596fd3efc9bf1e6c503f3ba2b
damien <damien0325@gmail.com>
2021-02-19 00:17:48 +0800

    correct definitions

--- src/main/kotlin/com/noodle/upper/utility/FlowHelper.kt
+++ src/main/kotlin/com/noodle/upper/utility/FlowHelper.kt
@@ -8,13 +8,13 @@ import org.springframework.http.codec.ServerSentEvent
 
 object FlowHelper {
     fun <T> progress(flowSupplier: ()-> Flow<T>, expected: Int, data: Boolean=false) = flow {
-        var retrieved = 0F
-        val invoiceFlow = flowSupplier()
+        var retrieved = 0
+        val invoiceFlow: Flow<T> = flowSupplier()
                 .onEach{
-                    val prevCompletion = ((retrieved)/expected * 100).toInt()
-                    val completion = ((retrieved+1)/expected * 100).toInt()
+                    val prevCompletion = ((retrieved).toFloat()/expected * 100).toInt()
+                    val completion = ((retrieved+1).toFloat()/expected * 100).toInt()
                     if (completion!=prevCompletion) emit(ServerSentEvent.builder<T>().comment("$completion%").id("progress").build())
-                    if (data) emit(ServerSentEvent.builder<T>(it).build())
+                    if (data) emit(ServerSentEvent.builder(it).build())
                     retrieved++
                 }
         val invoices = invoiceFlow.toList()
