836589f9369150926838d5817006eea69f96a0a5
damien <damien0325@gmail.com>
2021-02-21 14:35:10 +0800

    prune endpoints

--- src/main/kotlin/com/noodle/upper/controllers/InvoiceController.kt
+++ src/main/kotlin/com/noodle/upper/controllers/InvoiceController.kt
@@ -5,7 +5,6 @@ import com.noodle.upper.models.Tracked
 import com.noodle.upper.services.ListService
 import com.noodle.upper.services.SearchService
 import com.noodle.upper.services.UploadService
-import io.swagger.v3.oas.annotations.Operation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
@@ -50,30 +49,4 @@ class InvoiceController(
                     .onEach{println("sending ${it.entity?.size}")}
                     .map{ServerSentEvent.builder(it).build()}
                     .catch{println(it)}
-    @CrossOrigin(origins = ["http://localhost:3000"])
-    @Operation(
-            summary = "Old search restful endpoint",
-            description = "queries the db using the given string using matchingAny criteria",
-            deprecated = true
-    )
-    @Deprecated("Error prone", ReplaceWith("searchService.searchWords(words).map { ServerSentEvent.builder(it).build() }.catch { println(it) }", "kotlinx.coroutines.flow.map", "org.springframework.http.codec.ServerSentEvent", "kotlinx.coroutines.flow.catch"))
-    @GetMapping("/words")
-    suspend fun searchString(
-            @RequestParam words: String):
-            Flow<ServerSentEvent<Tracked<List<Invoice>>>> =
-            searchService.searchWords(words)
-                    .map{ServerSentEvent.builder(it).build()}
-                    .catch{println(it)}
-    @FlowPreview
-    @Operation(
-            summary = "Old upload restful endpoint",
-            deprecated = true
-    )
-    @Deprecated("Error prone", ReplaceWith("uploadService.upload(invoiceCsv).onEach { println(it) }", "kotlinx.coroutines.flow.onEach"))
-    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun upload(
-            @RequestPart("invoiceCsv") invoiceCsv: FilePart):
-            Flow<ServerSentEvent<Map<String, Float>>> =
-            uploadService.upload(invoiceCsv)
-                    .onEach{println(it)}
 }
