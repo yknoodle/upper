d7a51f36a0426f34d944206b04dd54e6dac03e60
damien <damien0325@gmail.com>
2021-02-24 02:39:13 +0800

    clean up RepositoryTests.kt

--- src/test/kotlin/com/noodle/upper/RepositoryTests.kt
+++ src/test/kotlin/com/noodle/upper/RepositoryTests.kt
@@ -20,44 +20,41 @@ import org.springframework.data.mongodb.core.query.TextCriteria
  * Spring Data library, and should work as long as Spring is maintained
  */
 @SpringBootTest
-class RepositoryTests {
+class RepositoryTests(
+        @Autowired val invoiceRepository: ReactiveInvoiceRepository) {
     @BeforeEach
-    fun beforeEach(
-            @Autowired reactiveInvoiceRepository: ReactiveInvoiceRepository) {
-        reactiveInvoiceRepository.deleteAll().block()
+    fun beforeEach(){
+        invoiceRepository.deleteAll().block()
     }
     @Test
-    fun canInsertInvoice(
-            @Autowired reactiveInvoiceRepository: ReactiveInvoiceRepository) = runBlocking {
-        val inserted = reactiveInvoiceRepository.insert(Invoice()).asFlow().firstOrNull()
+    fun canInsertInvoice( ) = runBlocking {
+        val inserted = invoiceRepository.insert(Invoice()).asFlow().firstOrNull()
         assert(inserted != null)
     }
 
     @Test
     fun canTextQuery(
-            @Autowired reactiveInvoiceRepository: ReactiveInvoiceRepository) = runBlocking {
-        val inserted: Invoice? = reactiveInvoiceRepository.insert(Invoice(id = "someId", description = "some Description")).asFlow().firstOrNull()
-        val queried: Invoice? = reactiveInvoiceRepository.findAllBy(TextCriteria.forDefaultLanguage().matchingAny("some Description")).asFlow().firstOrNull()
+    ) = runBlocking {
+        val inserted: Invoice? = invoiceRepository.insert(Invoice(id = "someId", description = "some Description")).asFlow().firstOrNull()
+        val queried: Invoice? = invoiceRepository.findAllBy(TextCriteria.forDefaultLanguage().matchingAny("some Description")).asFlow().firstOrNull()
         assert(inserted?.description == queried?.description)
     }
 
     @Test
-    fun noResultQuery(
-            @Autowired reactiveInvoiceRepository: ReactiveInvoiceRepository) = runBlocking {
-        reactiveInvoiceRepository.insert(Invoice(id = "someId", description = "some Description")).asFlow().first()
-        val queried: Invoice? = reactiveInvoiceRepository.findAllBy(TextCriteria.forDefaultLanguage().matchingAny("abc")).asFlow().firstOrNull()
+    fun noResultQuery() = runBlocking {
+        invoiceRepository.insert(Invoice(id = "someId", description = "some Description")).asFlow().first()
+        val queried: Invoice? = invoiceRepository.findAllBy(TextCriteria.forDefaultLanguage().matchingAny("abc")).asFlow().firstOrNull()
         assert(queried == null)
     }
 
     @Test
-    fun pagedResultQuery(
-            @Autowired reactiveInvoiceRepository: ReactiveInvoiceRepository) = runBlocking {
-        reactiveInvoiceRepository.insert(listOf(
+    fun pagedResultQuery() = runBlocking {
+        invoiceRepository.insert(listOf(
                 Invoice(id = "someId", description = "some Description"),
                 Invoice(id = "someId2", description = "some Description2"),
                 Invoice(id = "someId3", description = "some Description3"))).asFlow().toList()
-        val queriedPage1: List<Invoice> = reactiveInvoiceRepository.findAllBy(PageRequest.of(0,2)).asFlow().toList()
-        val queriedPage2: List<Invoice> = reactiveInvoiceRepository.findAllBy(PageRequest.of(1,2)).asFlow().toList()
+        val queriedPage1: List<Invoice> = invoiceRepository.findAllBy(PageRequest.of(0,2)).asFlow().toList()
+        val queriedPage2: List<Invoice> = invoiceRepository.findAllBy(PageRequest.of(1,2)).asFlow().toList()
         assert(queriedPage1.size == 2)
         assert(queriedPage2.size == 1)
     }
