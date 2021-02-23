e53cf819bd177e1c8b302ce6bc2b800db7819f1a
damien <damien0325@gmail.com>
2021-02-23 22:09:28 +0800

    prune unused

--- src/main/kotlin/com/noodle/upper/utility/CsvReaderHelper.kt
+++ src/main/kotlin/com/noodle/upper/utility/CsvReaderHelper.kt
@@ -4,6 +4,13 @@ import com.opencsv.CSVReader
 import com.opencsv.bean.CsvToBean
 import com.opencsv.bean.CsvToBeanBuilder
 import com.opencsv.exceptions.CsvException
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.reactive.asFlow
+import org.springframework.core.io.buffer.DataBufferUtils
+import org.springframework.http.codec.multipart.FilePart
 import java.io.InputStream
 import java.io.InputStreamReader
 
@@ -21,3 +28,9 @@ inline fun <reified T> getCSVReader(
 inline fun <reified T> InputStream.asCsvToBean(
         noinline handle: ((e: CsvException) -> CsvException)?): CsvToBean<T> =
         getCSVReader(this, handle)
+
+@FlowPreview
+inline fun <reified T> FilePart.asFlow(
+        noinline handle: ((e: CsvException) -> CsvException)? = { it }): Flow<T> =
+        DataBufferUtils.join(this.content()).asFlow()
+                .flatMapConcat { it.asInputStream().asCsvToBean<T>(handle).iterator().asFlow() }
